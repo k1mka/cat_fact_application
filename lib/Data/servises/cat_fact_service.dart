9549552f513a77c14dc6f6c3a482a3290888828a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-01 15:35:43 +0300

    added response with date and return result

--- lib/Data/servises/cat_fact_service.dart
+++ lib/Data/servises/cat_fact_service.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
-
 import 'package:http/http.dart';
 
 abstract class CatFactService {
-  Future<String> fetchFacts();
+  Future<Map> fetchFacts();
 }
 
 class CatFactServiceImpl implements CatFactService {
   @override
-  Future<String> fetchFacts() async {
+  Future<Map> fetchFacts() async {
     final url = Uri.parse(
         'https://cat-fact.herokuapp.com/facts/random?animal_type=cat');
     final response = await get(url);
-    Map<String, dynamic> responseMapFacts = json.decode(response.body);
-    return responseMapFacts['text'];
+    Map<String, dynamic> responseMapFact = json.decode(response.body);
+    Map<String, dynamic> result = {};
+    result['fact'] = responseMapFact['text'];
+    result['date'] = responseMapFact['createdAt'];
+    return result;
+
     // TODO: DIVIDE API URL TO STATIC VALUE
+    // TODO: FORMAT DISPLAYED FACT AND DATA
+    // TODO: CONVERT STRING RESPONSE TO DATA TIME TYPE
   }
 }

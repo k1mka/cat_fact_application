39f7cb6937d7bc391b92707341dbf4c27af4e7b1
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-02 09:34:57 +0300

    added hive ini
    Took 21 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cat_fact_application/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'cat_trivia_app.dart';
 
-void main() {
+void main() async {
   setupServiceLocator();
+  await Hive.initFlutter();
   runApp(const CatTrivia());
 }
